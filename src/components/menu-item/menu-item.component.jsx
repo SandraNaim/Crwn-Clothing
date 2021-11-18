1b662ba009de78b89483bc51c961be079347841c
Sandra Naim <sandra.naim@toothpickapp.com>
2021-11-18 18:08:12 +0200

    on click push the path needed and wrap the menuitem with withRouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { withRouter } from "react-router";
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl }) => (
+    <div className={`${size} menu-item`} onClick={()=>history.push(`${linkUrl}`)} >
         <div className="background-image" style={{
             backgroundImage: `url(${imageUrl})`
         }}/>
@@ -13,4 +14,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem) // by wrapping the menuItem with the withrouter, now we have access to the history
