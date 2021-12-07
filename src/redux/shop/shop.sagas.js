beca2d48bb3ebdbaa8ca9efb883d11b980ce1af0
Sandra Naim <sandra.naim@toothpickapp.com>
2021-12-07 18:05:45 +0200

    install redux saga and move the shop logic from the shop action to the shop sagas file

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,23 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
+import { fetchCollectionsFailure, fetchCollectionsSuccess } from './shop.actions';
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        // in sagas we use 'put' as a dispatch
+        yield put(fetchCollectionsSuccess(collectionsMap))
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message))
+    }
+}
+
+export function* fetchCollectionsStart() {
+    yield takeEvery(
+        ShopActionTypes.FETCHE_COLLECTIONS_START,
+        fetchCollectionsAsync
+    )
+}
