f5ad5b1742ad8f538a603cb34dfcace98c829a9a
Sandra Naim <sandra.naim@toothpickapp.com>
2021-12-09 11:13:41 +0200

    dispatch check user session constant and trigger its function to check if the user is Authenticated

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,4 +1,4 @@
-import { auth, createUserProfileDocument, googleProvider } from "../../firebase/firebase.utils";
+import { auth, createUserProfileDocument, getCurrentUser, googleProvider } from "../../firebase/firebase.utils";
 import { takeLatest, put, all, call } from "redux-saga/effects";
 import UserActionTypes from "./user.types";
 import { signInFailure, signInSuccess } from "./user.actions";
@@ -27,6 +27,19 @@ export function* signInWithEmail({payload: { email, password }}) {
     }
 }
 
+export function* isUserAuthenticated() {
+    try {
+        const userAuth = yield getCurrentUser();
+        if(!userAuth) {
+            return
+        } else {
+            yield getSnapshotFromUserAuth(userAuth)
+        }
+    } catch (error) {
+        yield put(signInFailure(error));
+    }
+}
+
 export function* onGoogleSignInStart() {
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -35,6 +48,10 @@ export function* onEmailSignInStart() {
     yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
+export function* onCheckUserSession() {
+    yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
 export function* userSagas() {
-    yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
+    yield all([call(onGoogleSignInStart), call(onEmailSignInStart), call(onCheckUserSession)]);
 }
